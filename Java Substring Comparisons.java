8f8635404992f977f4e1a5bb5b80d2da30ebe92e
Piyush <44377484+piyush19198@users.noreply.github.com>
2020-07-19 11:26:31 +0530

    Create Java Substring Comparisons.java

---
+++ Java Substring Comparisons.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest  = s.substring(0, k);
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            String curr = s.substring(i, i + k);
+            if (smallest.compareTo(curr) > 0){
+                smallest = curr;
+            }
+            if (largest.compareTo(curr) < 0) {
+                largest = curr;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
